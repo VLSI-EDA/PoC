b76298a9903ddf9d349d1eb00b3f6dd38e351a75
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2015-08-22 04:02:21 +0200

    Removed PoC.board from *.files files. Adjusted compile order in *.files files.

--- tb/common/strings_tb.vhdl
+++ tb/common/strings_tb.vhdl
@@ -49,13 +49,70 @@ architecture tb of strings_tb is
 	constant raw_format_slv_dec_result2		: STRING		:= raw_format_slv_dec(x"12345678");
 	constant raw_format_slv_dec_result3		: STRING		:= raw_format_slv_dec(x"A1B2C3D4E5F607A8");
 	
+	constant str_length_result0						: INTEGER		:= str_length("");
+	constant str_length_result1						: INTEGER		:= str_length((1 to 3 => C_POC_NUL));
+	constant str_length_result2						: INTEGER		:= str_length("Hello");
+	constant str_length_result3						: INTEGER		:= str_length("Hello" & (1 to 3 => C_POC_NUL));
+	
+	constant str_match_result0						: BOOLEAN		:= str_match("", "");
+	constant str_match_result1						: BOOLEAN		:= str_match("", (1 to 3 => C_POC_NUL));
+	constant str_match_result2						: BOOLEAN		:= str_match("Hello", "hello");
+	constant str_match_result3						: BOOLEAN		:= str_match("Hello", "Hello");
+	constant str_match_result4						: BOOLEAN		:= str_match("Hello World", "Hello");
+	constant str_match_result5						: BOOLEAN		:= str_match("Hello", "Hello World");
+	constant str_match_result6						: BOOLEAN		:= str_match("Hello", "Hello" & (1 to 3 => C_POC_NUL));
+	
+	constant str_imatch_result0						: BOOLEAN		:= str_imatch("", "");
+	constant str_imatch_result1						: BOOLEAN		:= str_imatch("", (1 to 3 => C_POC_NUL));
+	constant str_imatch_result2						: BOOLEAN		:= str_imatch("Hello", "hello");
+	constant str_imatch_result3						: BOOLEAN		:= str_imatch("Hello", "Hello");
+	constant str_imatch_result4						: BOOLEAN		:= str_imatch("Hello World", "Hello");
+	constant str_imatch_result5						: BOOLEAN		:= str_imatch("Hello", "Hello World");
+	constant str_imatch_result6						: BOOLEAN		:= str_imatch("Hello", "Hello" & (1 to 3 => C_POC_NUL));
 begin
 	process
 	begin
-		tbAssert((raw_format_slv_dec_result0 = "18"),										"raw_format_slv_dec(0x12)="	& raw_format_slv_dec_result0 &	"    Expected='18'");
-		tbAssert((raw_format_slv_dec_result1 = "13398"),								"raw_format_slv_dec(0x3456)="	& raw_format_slv_dec_result1 &	"    Expected='13398'");
-		tbAssert((raw_format_slv_dec_result2 = "305419896"),						"raw_format_slv_dec(0x12345678)="	& raw_format_slv_dec_result2 &	"    Expected='305419896'");
-		tbAssert((raw_format_slv_dec_result3 = "11651590505119483816"),	"raw_format_slv_dec(0xA1b2c3d4e5f607a8)="	& raw_format_slv_dec_result3 &	"    Expected='11651590505119483816'");
+		-- raw_format_slv_dec tests
+		tbAssert((raw_format_slv_dec_result0 = "18"),										"raw_format_slv_dec(0x12)="								& raw_format_slv_dec_result0	&	"    Expected='18'");
+		tbAssert((raw_format_slv_dec_result1 = "13398"),								"raw_format_slv_dec(0x3456)="							& raw_format_slv_dec_result1	&	"    Expected='13398'");
+		tbAssert((raw_format_slv_dec_result2 = "305419896"),						"raw_format_slv_dec(0x12345678)="					& raw_format_slv_dec_result2	&	"    Expected='305419896'");
+		tbAssert((raw_format_slv_dec_result3 = "11651590505119483816"),	"raw_format_slv_dec(0xA1b2c3d4e5f607a8)="	& raw_format_slv_dec_result3	&	"    Expected='11651590505119483816'");
+		
+		-- str_length tests
+		tbAssert((str_length_result0 = 0),			"str_length('')="											& INTEGER'image(str_length_result0)	& "  Expected=0");
+		tbAssert((str_length_result1 = 0),			"str_length('\0\0\0')="								& INTEGER'image(str_length_result1)	& "  Expected=0");
+		tbAssert((str_length_result2 = 5),			"str_length('Hello')="								& INTEGER'image(str_length_result2)	& "  Expected=5");
+		tbAssert((str_length_result3 = 5),			"str_length('Hello\0\0\0')="					& INTEGER'image(str_length_result3)	& "  Expected=5");
+		
+		-- str_match tests
+		tbAssert((str_match_result0 = TRUE),		"str_match('', '')="									& BOOLEAN'image(str_match_result0)	& "  Expected=TRUE");
+		tbAssert((str_match_result1 = TRUE),		"str_match('', '\0\0\0')="						& BOOLEAN'image(str_match_result1)	& "  Expected=TRUE");
+		tbAssert((str_match_result2 = FALSE),		"str_match('Hello', 'hello')="				& BOOLEAN'image(str_match_result2)	& "  Expected=FALSE");
+		tbAssert((str_match_result3 = TRUE),		"str_match('Hello', 'Hello')="				& BOOLEAN'image(str_match_result3)	& "  Expected=TRUE");
+		tbAssert((str_match_result4 = FALSE),		"str_match('Hello World', 'Hello')="	& BOOLEAN'image(str_match_result4)	& "  Expected=FALSE");
+		tbAssert((str_match_result5 = FALSE),		"str_match('Hello', 'Hello World')="	& BOOLEAN'image(str_match_result5)	& "  Expected=FALSE");
+		tbAssert((str_match_result6 = TRUE),		"str_match('Hello', 'Hello\0\0\0')="	& BOOLEAN'image(str_match_result6)	& "  Expected=TRUE");
+		
+		-- str_imatch tests
+		tbAssert((str_imatch_result0 = TRUE),		"str_imatch('', '')="									& BOOLEAN'image(str_imatch_result0) & "  Expected=TRUE");
+		tbAssert((str_imatch_result1 = TRUE),		"str_imatch('', '\0\0\0')="						& BOOLEAN'image(str_imatch_result1) & "  Expected=TRUE");
+		tbAssert((str_imatch_result2 = TRUE),		"str_imatch('Hello', 'hello')="				& BOOLEAN'image(str_imatch_result2) & "  Expected=TRUE");
+		tbAssert((str_imatch_result3 = TRUE),		"str_imatch('Hello', 'Hello')="				& BOOLEAN'image(str_imatch_result3) & "  Expected=TRUE");
+		tbAssert((str_imatch_result4 = FALSE),	"str_imatch('Hello World', 'Hello')="	& BOOLEAN'image(str_imatch_result4) & "  Expected=FALSE");
+		tbAssert((str_imatch_result5 = FALSE),	"str_imatch('Hello', 'Hello World')="	& BOOLEAN'image(str_imatch_result5) & "  Expected=FALSE");
+		tbAssert((str_imatch_result6 = TRUE),		"str_imatch('Hello', 'Hello\0\0\0')="	& BOOLEAN'image(str_imatch_result6) & "  Expected=TRUE");
+
+		-- str_pos tests
+		-- str_ipos tests
+		-- str_find tests
+		-- str_ifind tests
+		-- str_replace tests
+		-- str_substr tests
+		-- str_ltrim tests
+		-- str_rtrim tests
+		-- str_trim tests
+		-- str_toLower tests
+		-- str_toUpper tests
 		
 		-- simulation completed
 		
