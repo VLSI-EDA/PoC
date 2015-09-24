5454d985acffd40beb961e6dc4bd49beb92c62c9
Thomas B. Preußer <thomas.preusser@tu-dresden.de>
2015-09-24 08:10:07 +0200

    Rewrite of ocram_sdp: more plausibility tests on reads and writes during simulation; isolation of initialization from file into procedure tested with ISE, Quartus and Vivado -> code duplication eliminated; uninitialized RAM is loaded with all 'U' by default. Publish interface controller for the communication via a FT245 USB-Serial converter by Peter Reichel, Jan Schirok and Steffen Koehler.

---
+++ src/io/uart/uart_ft245.vhdl
@@ -0,0 +1,260 @@
+-- EMACS settings: -*-  tab-width: 2; indent-tabs-mode: t -*-
+-- vim: tabstop=2:shiftwidth=2:noexpandtab
+-- kate: tab-width 2; replace-tabs off; indent-width 2;
+-- ===========================================================================
+--
+-- Authors:     Peter Reichel
+--              Jan Schirok
+--              Steffen Koehler
+--
+-- Module:      UART controller for FTDI FT245M UART-over-USB converter.
+--
+-- License:
+-- ===========================================================================
+-- Copyright 2008-2015 Technische Universitaet Dresden - Germany
+--										 Chair for VLSI-Design, Diagnostics and Architecture
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--		http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- ===========================================================================
+
+
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity uart_ft245 is
+   generic (
+      CLK_FREQ : positive
+   );
+   port (
+      -- common signals
+      clk : in std_logic;
+      rst : in std_logic;
+
+      -- send data
+      snd_ready  : out std_logic;
+      snd_strobe : in  std_logic;
+      snd_data   : in  std_logic_vector(7 downto 0);
+
+      -- receive data
+      rec_strobe : out std_logic;
+      rec_data   : out std_logic_vector(7 downto 0);
+
+      -- connection to ft245
+      ft245_data   : inout std_logic_vector(7 downto 0);
+      ft245_rdn    : out   std_logic;
+      ft245_wrn    : out   std_logic;
+      ft245_rxfn   : in    std_logic;
+      ft245_txen   : in    std_logic;
+      ft245_pwrenn : in    std_logic
+   );
+end entity;
+
+
+library IEEE;
+use IEEE.numeric_std.all;
+
+library PoC;
+use PoC.utils.all;
+
+architecture rtl of uart_ft245 is
+
+   -- clock frequency (MHz)
+   constant CLK_FREQ_MHZ : integer := CLK_FREQ / 1000000;
+
+   -- FT245 communication delay cycles (minimum delay is 50 ns = 1/20 us)
+   constant DELAY_CYCLES : integer := CLK_FREQ_MHZ / 20;
+
+   -- delay register width
+   constant DELAY_WIDTH : integer := log2ceilnz(DELAY_CYCLES + 1);
+
+   -- delay register load value
+   constant DELAY_LOAD : unsigned(DELAY_WIDTH-1 downto 0) :=
+      to_unsigned(DELAY_CYCLES, DELAY_WIDTH);
+
+   -- delay register
+   signal reg_delay    : unsigned(DELAY_WIDTH-1 downto 0);
+
+   -- FSM
+   type tState is ( IDLE, RD1, RD2, RD3, RD4, WR1, WR2, WR3, WR4 );
+   signal fsm_state : tState := IDLE;
+   signal fsm_nextstate : tState;
+
+   -- registers
+   signal reg_data_snd  : std_logic_vector(7 downto 0);
+   signal reg_data_rec  : std_logic_vector(7 downto 0);
+   signal reg_ld_rec    : std_logic;
+   signal reg_dto_b     : std_logic := '1';    -- low-active
+   signal reg_wr_b      : std_logic := '1';    -- low-active
+   signal reg_rd_b      : std_logic := '1';    -- low-active
+
+   signal ff_susp       : std_logic := '1';    -- low-active
+   signal ff_rxf        : std_logic := '1';    -- low-active
+   signal ff_txe        : std_logic := '1';    -- low-active
+
+   -- control signals
+   signal ctrl_ld_rec   : std_logic;
+   signal ctrl_delay    : std_logic;
+   signal ctrl_rd       : std_logic;
+   signal ctrl_wr       : std_logic;
+   signal ctrl_dto      : std_logic;
+
+   signal data_in       : std_logic_vector(7 downto 0);
+
+begin
+   ----------------------------------------------
+   -- Synchronize Inputs
+   process(clk)
+   begin
+      if rising_edge(clk) then
+        if rst = '1' then
+          -- Neutral PowerUp / Reset
+          ff_susp <= '1';
+          ff_rxf  <= '1';
+          ff_txe  <= '1';
+        else
+          -- Wait for Initilization to Complete
+          ff_susp <= ft245_pwrenn;
+
+          -- Now forward Fill Signals
+          ff_rxf  <= ft245_rxfn;
+          ff_txe  <= ft245_txen;
+        end if;
+      end if;
+   end process;
+
+   process(fsm_state, snd_strobe, reg_delay, ff_susp, ff_rxf, ff_txe)
+   begin
+      fsm_nextstate <= fsm_state;
+      ctrl_ld_rec <= '0';
+      ctrl_rd <= '0';
+      ctrl_wr <= '0';
+      ctrl_dto <= '0';
+      ctrl_delay <= '0';
+
+      case fsm_state is
+         when IDLE =>
+            if ff_susp = '0' then
+               if ff_rxf = '0' then
+                  -- receive data
+                  fsm_nextstate <= RD1;
+               elsif ff_txe = '0' and snd_strobe = '1' then
+                  -- ok, send...
+                  fsm_nextstate <= WR1;
+               end if;
+            end if;
+         when RD1 =>
+            -- load delay counter
+            ctrl_rd <= '1';
+            ctrl_delay <= '1';
+            fsm_nextstate <= RD2;
+         when RD2 =>
+            -- wait until delay counter has expired
+            ctrl_rd <= '1';
+            if reg_delay = 0 then
+               fsm_nextstate <= RD3;
+            end if;
+         when RD3 =>
+            -- data is valid now => load
+            ctrl_rd <= '1';
+            ctrl_ld_rec <= '1';
+            -- load delay counter again
+            ctrl_delay <= '1';
+            fsm_nextstate <= RD4;
+         when RD4 =>
+            -- wait until delay counter has expired
+            if reg_delay = 0 then
+               fsm_nextstate <= IDLE;
+            end if;
+
+         when WR1 =>
+            -- load delay counter
+            ctrl_dto <= '1';
+            ctrl_delay <= '1';
+            fsm_nextstate <= WR2;
+         when WR2 =>
+            -- set wr (active pulse)
+            ctrl_dto <= '1';
+            ctrl_wr <= '1';
+            -- wait until delay counter has expired
+            if reg_delay = 0 then
+               fsm_nextstate <= WR3;
+            end if;
+         when WR3 =>
+            -- clear wr (pre-charge time)
+            ctrl_dto <= '1';
+            -- load delay counter again
+            ctrl_delay <= '1';
+            fsm_nextstate <= WR4;
+         when WR4 =>
+            -- wait until delay counter has expired
+            if reg_delay = 0 then
+               fsm_nextstate <= IDLE;
+            end if;
+      end case;
+   end process;
+
+   ----------------------------------------------
+   -- registers
+   process(clk)
+   begin
+      if rising_edge(clk) then
+
+         -- control signals
+         if rst = '1' then
+            fsm_state   <= IDLE;
+            reg_rd_b    <= '1';
+            reg_wr_b    <= '1';
+            reg_dto_b   <= '1';
+            reg_ld_rec  <= '0';
+         else
+            fsm_state   <= fsm_nextstate;
+            reg_rd_b    <= not ctrl_rd;
+            reg_wr_b    <= not ctrl_wr;
+            reg_dto_b   <= not ctrl_dto;
+            reg_ld_rec  <= ctrl_ld_rec;
+         end if;
+
+         -- delay counter
+         if ctrl_delay = '1' then
+            reg_delay <= DELAY_LOAD;
+         else
+            reg_delay <= reg_delay - 1;
+         end if;
+
+         -- received data
+         if ctrl_ld_rec = '1' then
+            reg_data_rec <= data_in;
+         end if;
+
+         -- data to send
+         if snd_strobe = '1' then
+            reg_data_snd <= snd_data;
+         end if;
+
+      end if;
+   end process;
+
+   ----------------------------------------------
+   -- tristate driver and output assignments
+   ft245_data <= reg_data_snd when reg_dto_b = '0' else (others => 'Z');
+   data_in <= ft245_data;
+
+   ft245_rdn <= reg_rd_b;
+   ft245_wrn <= reg_wr_b;
+
+   rec_data   <= reg_data_rec;
+   rec_strobe <= reg_ld_rec;
+   snd_ready  <= ff_rxf and not ff_txe and not ff_susp
+      when fsm_state = IDLE else '0';
+
+end rtl;
--- src/mem/ocram/ocram_sdp.vhdl
+++ src/mem/ocram/ocram_sdp.vhdl
@@ -1,20 +1,20 @@
 -- EMACS settings: -*-  tab-width: 2; indent-tabs-mode: t -*-
 -- vim: tabstop=2:shiftwidth=2:noexpandtab
 -- kate: tab-width 2; replace-tabs off; indent-width 2;
--- 
+--
 -- ============================================================================
 -- Authors:				 	Martin Zabel
 --									Thomas B. Preusser
 --									Patrick Lehmann
 --
 -- Module:				 	Simple dual-port memory.
--- 
+--
 -- Description:
 -- ------------------------------------
 -- Inferring / instantiating simple dual-port memory, with:
 --	* dual clock, clock enable,
 --	* 1 read port plus 1 write port.
--- 
+--
 -- The generalized behavior across Altera and Xilinx FPGAs since
 -- Stratix/Cyclone and Spartan-3/Virtex-5, respectively, is as follows:
 --
@@ -36,18 +36,18 @@
 --
 -- TODO: add timing diagram
 -- TODO: implement correct behavior for RT-level simulation
--- 
+--
 -- License:
 -- ============================================================================
 -- Copyright 2008-2015 Technische Universitaet Dresden - Germany
 --										 Chair for VLSI-Design, Diagnostics and Architecture
--- 
+--
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
 -- You may obtain a copy of the License at
--- 
+--
 --		http://www.apache.org/licenses/LICENSE-2.0
--- 
+--
 -- Unless required by applicable law or agreed to in writing, software
 -- distributed under the License is distributed on an "AS IS" BASIS,
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -55,19 +55,9 @@
 -- limitations under the License.
 -- ============================================================================
 
-library STD;
-use			STD.TextIO.all;
-
 library	IEEE;
 use			IEEE.std_logic_1164.all;
 use			IEEE.numeric_std.all;
-use			IEEE.std_logic_textio.all;
-
-library PoC;
-use			PoC.config.all;
-use			PoC.utils.all;
-use			PoC.strings.all;
-
 
 entity ocram_sdp is
 	generic (
@@ -88,11 +78,21 @@ entity ocram_sdp is
 end entity;
 
 
-architecture rtl of ocram_sdp is
-	attribute ramstyle	: string;
+library std;
+use			std.TextIO.all;
 
-	constant DEPTH			: positive := 2**A_BITS;
-	
+library	IEEE;
+use			IEEE.std_logic_textio.all;
+
+library PoC;
+use			PoC.config.all;
+use			PoC.utils.all;
+use			PoC.strings.all;
+
+architecture rtl of ocram_sdp is
+
+  constant DEPTH : positive := 2**A_BITS;
+
 begin
 
 	gInfer: if VENDOR = VENDOR_XILINX or VENDOR = VENDOR_ALTERA generate
@@ -106,19 +106,18 @@ begin
 		--	 This is the expected behaviour.
 		--	 With two different clocks, synthesis complains about an undefined
 		--	 read-write behaviour, that can be ignored.
+    subtype word_t is std_logic_vector(D_BITS - 1 downto 0);
+    type ram_t is array(0 to DEPTH - 1) of word_t;
+    attribute ramstyle : string;
 
-		subtype word_t	is std_logic_vector(D_BITS - 1 downto 0);
-		type		ram_t		is array(0 to DEPTH - 1) of word_t;
-		
-	begin
-		genLoadFile : if (str_length(FileName) /= 0) generate
-			-- Read a *.mem or *.hex file
-			impure function ocram_ReadMemFile(FileName : STRING) return ram_t is
-				file FileHandle				: TEXT open READ_MODE is FileName;
-				variable CurrentLine	: LINE;
-				variable TempWord			: STD_LOGIC_VECTOR((div_ceil(word_t'length, 4) * 4) - 1 downto 0);
-				variable Result				: ram_t		:= (others => (others => '0'));
-				
+		-- Compute the initialization of a RAM array, if specified, from the passed file.
+    impure function ocram_InitMemory(FileName : string) return ram_t is
+
+			-- Read the specified file name into the RAM array.
+			procedure ReadMemFile(FileName : in string; variable mem : inout ram_t) is
+				file FileHandle      : TEXT open READ_MODE is FileName;
+				variable CurrentLine : LINE;
+				variable TempWord    : STD_LOGIC_VECTOR((div_ceil(word_t'length, 4) * 4) - 1 downto 0);
 			begin
 				-- discard the first line of a mem file
 				if (str_toLower(FileName(FileName'length - 3 to FileName'length)) = ".mem") then
@@ -130,77 +129,68 @@ begin
 
 					readline(FileHandle, CurrentLine);
 					hread(CurrentLine, TempWord);
-					Result(i)		:= resize(TempWord, word_t'length);
+					mem(i) := TempWord(word_t'range);
 				end loop;
+			end;
 
-				return Result;
-			end function;
-
-			signal ram								: ram_t		:= ocram_ReadMemFile(FILENAME);
-			attribute ramstyle of ram	: signal is "no_rw_check";
-			
+			variable res : ram_t := (others => (others => 'U'));
 		begin
-			process (wclk)
-			begin
-				if rising_edge(wclk) then
-					if (wce and we) = '1' then
-						ram(to_integer(wa)) <= d;
-					end if;
-				end if;
-			end process;
+			if str_length(FileName) > 0 then
+				ReadMemFile(FileName, res);
+			end if;
+			return  res;
+		end ocram_InitMemory;
 
-			process (rclk)
-			begin
-				if rising_edge(rclk) then
-					-- read data doesn't care, when reading at write address
-					if rce = '1' then
-					--synthesis translate_off
-						if Is_X(std_logic_vector(ra)) then
-							q <= (others => 'X');
-						else
-					--synthesis translate_on
-							q <= ram(to_integer(ra));
-					--synthesis translate_off
-						end if;
-					--synthesis translate_on
-					end if;
-				end if;
-			end process;
-		end generate;
-		genNoLoadFile : if (str_length(FileName) = 0) generate
-			signal ram								: ram_t;
-			attribute ramstyle of ram	: signal is "no_rw_check";
-			
-		begin
-			process (wclk)
-			begin
-				if rising_edge(wclk) then
-					if (wce and we) = '1' then
-						ram(to_integer(wa)) <= d;
-					end if;
-				end if;
-			end process;
+    signal ram : ram_t := ocram_InitMemory(FILENAME);
+    attribute ramstyle of ram : signal is "no_rw_check";
 
-			process (rclk)
-			begin
-				if rising_edge(rclk) then
-					-- read data doesn't care, when reading at write address
-					if rce = '1' then
-					--synthesis translate_off
-						if Is_X(std_logic_vector(ra)) then
-							q <= (others => 'X');
-						else
+  begin
+    process(wclk)
+    begin
+      if rising_edge(wclk) then
+        if (wce and we) = '1' then
+          -- Note: Hide plausibility tests from synthesis to ensure
+          --       proper RAM inference.
+          --synthesis translate_off
+          if Is_X(std_logic_vector(wa)) then
+            report "ocram_sdp: Writing to ill-defined address."
+              severity error;
+          else
 					--synthesis translate_on
-							q <= ram(to_integer(ra));
-					--synthesis translate_off
-						end if;
+            ram(to_integer(wa)) <= d;
+          --synthesis translate_off
+          end if;
+				  --synthesis translate_on
+        end if;
+      end if;
+    end process;
+
+    process(rclk)
+    begin
+      if rising_edge(rclk) then
+        if rce = '1' then
+          -- Note: Hide plausibility tests from synthesis to ensure
+          --       proper RAM inference.
+          --synthesis translate_off
+          if Is_X(std_logic_vector(ra)) then
+            q <= (others => 'X');
+          elsif ra = wa then
+            -- read data unknown when reading at write address
+            q <= (others => 'X');
+            report "ocram_sdp: Reading from address just writing: Unknown result."
+              severity warning;
+          else
 					--synthesis translate_on
-					end if;
-				end if;
-			end process;
-		end generate;
+            q <= ram(to_integer(ra));
+          --synthesis translate_off
+          end if;
+				  --synthesis translate_on
+        end if;
+      end if;
+    end process;
+
 	end generate gInfer;
-	
+
 	assert VENDOR = VENDOR_XILINX or VENDOR = VENDOR_ALTERA
 		report "Device not yet supported."
 		severity failure;
