fe1b6700ae597aed82fcdd6a410e0e01f438dc98
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-12-15 19:53:41 +0100

    Testing new error reporting in poc.setup.sh for Travis-CI.

--- tools/Travis-CI/poc.setup.sh
+++ tools/Travis-CI/poc.setup.sh
@@ -7,13 +7,17 @@ MAGENTA='\e[1;35m'	# Magenta
 CYAN='\e[1;36m'			# Cyan
 NOCOLOR='\e[0m'			# No Color
 
+ERROR_COUNT=0
+
+# -> ERROR_COUNT
 # -> LastExitCode
 # -> Error message
 ExitIfError() {
-	if [ $1 -ne 0 ]; then
-		echo 1>&2 -e $2
-		exit 1
+	if [ $2 -ne 0 ]; then
+		echo 1>&2 -e $3
+		return $ERROR_COUNT+1
 	fi
+	return $ERROR_COUNT
 }
 
 echo -e "${MAGENTA}========================================${NOCOLOR}"
@@ -22,27 +26,35 @@ echo -e "${MAGENTA}========================================${NOCOLOR}"
 
 echo -e "${CYAN}Copy config.private.ini into ./py directory${NOCOLOR}"
 cp ./tools/Travis-CI/config.private.ini ./py
-ExitIfError $? "${RED}Copying of ./tools/Travis-CI/config.private.ini [FAILED]${NOCOLOR}"
+ERROR_COUNT=ExitIfError $ERROR_COUNT $? "${RED}Copying of ./tools/Travis-CI/config.private.ini [FAILED]${NOCOLOR}"
 
 echo -e "${CYAN}Copy my_project.vhdl into ./tb/common directory${NOCOLOR}"
 cp ./tools/Travis-CI/my_project.vhdl ./tb/common
-ExitIfError $? "${RED}Copying of ./tools/Travis-CI/my_project.vhdl [FAILED]${NOCOLOR}"
+ERROR_COUNT=ExitIfError $ERROR_COUNT $? "${RED}Copying of ./tools/Travis-CI/my_project.vhdl [FAILED]${NOCOLOR}"
 
 echo -e "${CYAN}Copy modelsim.ini into ./temp/precompiled/vsim directory${NOCOLOR}"
 mkdir -p ./temp/precompiled/vsim
-ExitIfError $? "${RED}Creating directory ./temp/precompiled/vsim [FAILED]${NOCOLOR}"
+ERROR_COUNT=ExitIfError $ERROR_COUNT $? "${RED}Creating directory ./temp/precompiled/vsim [FAILED]${NOCOLOR}"
 cp ./tools/Travis-CI/modelsim.ini ./temp/precompiled/vsim
-ExitIfError $? "${RED}Copying of ./tools/Travis-CI/modelsim.ini [FAILED]${NOCOLOR}"
+ERROR_COUNT=ExitIfError $ERROR_COUNT $? "${RED}Copying of ./tools/Travis-CI/modelsim.ini [FAILED]${NOCOLOR}"
 
 echo -e "${CYAN}Test PoC front-end script.${NOCOLOR}"
 ./poc.sh
-ExitIfError $? "${RED}Testing PoC front-end script [FAILED]${NOCOLOR}"
+ERROR_COUNT=ExitIfError $ERROR_COUNT $? "${RED}Testing PoC front-end script [FAILED]${NOCOLOR}"
 
 echo -e "${CYAN}Pre-compiling OSVVM for GHDL into ./temp/precompiled/ghdl/osvvm directory${NOCOLOR}"
 ./tools/precompile/compile-osvvm.sh --ghdl
-ExitIfError $? "${RED}Pre-compiling OSVVM for GHDL [FAILED]${NOCOLOR}"
+ERROR_COUNT=ExitIfError $ERROR_COUNT $? "${RED}Pre-compiling OSVVM for GHDL [FAILED]${NOCOLOR}"
 
 echo -e "${CYAN}Pre-compiling UVVM for GHDL into ./temp/precompiled/ghdl/uvvm directory${NOCOLOR}"
 echo -e "${RED}UVVM pre-compile scripts are not yet shipped with GHDL${NOCOLOR}"
 #./tools/precompile/compile-uvvm.sh --ghdl
 #ExitIfError $? "${RED}Pre-compiling UVVM for GHDL [FAILED]${NOCOLOR}"
+
+if [ $ERROR_COUNT -gt 0 ]; then
+	echo -e "Configuring PoC ${RED}[FAILED]${NOCOLOR}"
+	exit 1
+else
+	echo -e "Configuring PoC ${GREEN}[SUCESSFUL]${NOCOLOR}"
+	exit 0
+fi
