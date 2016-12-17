be92c63d03f2489eba3490033e7613b428a3e99a
Patrick Lehmann <Patrick.Lehmann@tu-dresden.de>
2016-12-17 17:09:45 +0100

    Added missing $-sign.

--- tools/AppVeyor/PoC.dryrun.ps1
+++ tools/AppVeyor/PoC.dryrun.ps1
@@ -6,39 +6,39 @@ Add-AppveyorTest -Name "Dryrun test: Active-HDL run for PoC.arith.prng" -Framewo
 $start = Get-Date
 .\PoC.ps1 --dryrun asim PoC.arith.prng
 $end = Get-Date
-Update-AppveyorTest -Name "Dryrun test: Active-HDL run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome (if ($LastExitCode -eq 0) {"Passed"} else {"Failed"})	-Duration ($end - $start).TotalSeconds
+Update-AppveyorTest -Name "Dryrun test: Active-HDL run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome $(if ($LastExitCode -eq 0) {"Passed"} else {"Failed"}) -Duration ($end - $start).TotalSeconds
 
 # ==============================================================================
 Add-AppveyorTest -Name "Dryrun test: Riviera-PRO run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome Running
 $start = Get-Date
 .\PoC.ps1 --dryrun rpro PoC.arith.prng
 $end = Get-Date
-Update-AppveyorTest -Name "Dryrun test: Riviera-PRO run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome (if ($LastExitCode -eq 0) {"Passed"} else {"Failed"})	-Duration ($end - $start).TotalSeconds
+Update-AppveyorTest -Name "Dryrun test: Riviera-PRO run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome $(if ($LastExitCode -eq 0) {"Passed"} else {"Failed"}) -Duration ($end - $start).TotalSeconds
 
 # ==============================================================================
 Add-AppveyorTest -Name "Dryrun test: GHDL run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome Running
 $start = Get-Date
 .\PoC.ps1 --dryrun ghdl PoC.arith.prng
 $end = Get-Date
-Update-AppveyorTest -Name "Dryrun test: GHDL run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome (if ($LastExitCode -eq 0) {"Passed"} else {"Failed"})	-Duration ($end - $start).TotalSeconds
+Update-AppveyorTest -Name "Dryrun test: GHDL run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome $(if ($LastExitCode -eq 0) {"Passed"} else {"Failed"}) -Duration ($end - $start).TotalSeconds
 
 # ==============================================================================
 Add-AppveyorTest -Name "Dryrun test: ModelSim run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome Running
 $start = Get-Date
 .\PoC.ps1 --dryrun vsim PoC.arith.prng
 $end = Get-Date
-Update-AppveyorTest -Name "Dryrun test: ModelSim run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome (if ($LastExitCode -eq 0) {"Passed"} else {"Failed"})	-Duration ($end - $start).TotalSeconds
+Update-AppveyorTest -Name "Dryrun test: ModelSim run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome $(if ($LastExitCode -eq 0) {"Passed"} else {"Failed"}) -Duration ($end - $start).TotalSeconds
 
 # ==============================================================================
 Add-AppveyorTest -Name "Dryrun test: ISE Simulator run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome Running
 $start = Get-Date
 .\PoC.ps1 --dryrun isim PoC.arith.prng
 $end = Get-Date
-Update-AppveyorTest -Name "Dryrun test: ISE Simulator run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome (if ($LastExitCode -eq 0) {"Passed"} else {"Failed"})	-Duration ($end - $start).TotalSeconds
+Update-AppveyorTest -Name "Dryrun test: ISE Simulator run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome $(if ($LastExitCode -eq 0) {"Passed"} else {"Failed"}) -Duration ($end - $start).TotalSeconds
 
 # ==============================================================================
 Add-AppveyorTest -Name "Dryrun test: Vivado Simulator run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome Running
 $start = Get-Date
 .\PoC.ps1 --dryrun xsim PoC.arith.prng
 $end = Get-Date
-Update-AppveyorTest -Name "Dryrun test: Vivado Simulator run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome (if ($LastExitCode -eq 0) {"Passed"} else {"Failed"})	-Duration ($end - $start).TotalSeconds
+Update-AppveyorTest -Name "Dryrun test: Vivado Simulator run for PoC.arith.prng" -Framework "PoC" -FileName "PoC.arith.prng" -Outcome $(if ($LastExitCode -eq 0) {"Passed"} else {"Failed"}) -Duration ($end - $start).TotalSeconds
